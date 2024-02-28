52db8cb786621bf751a2d7b207148f6b0cf6fe89
anhf0309 <anhf0309@gmail.com>
2024-02-28 01:38:14 -0800

    Set Up SpringBoot

--- src/main/java/com/orgin/model/RestaurantWebApplication.java
+++ src/main/java/com/orgin/model/RestaurantWebApplication.java
@@ -6,6 +6,6 @@ import org.springframework.boot.SpringApplication;
 @SpringBootApplication
 public class RestaurantWebApplication {
     public static void main(String[] args) {
-        SpringApplication.run(RestaurantWebApplication.class, args); 
+        SpringApplication.run(RestaurantWebApplication.class, args);
     }
 }
